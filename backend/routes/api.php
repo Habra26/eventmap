4d673e3ae242755ef73118a040db13fe170bfe5a
Habra26 <arnaud.habraken@outlook.com>
2026-04-23 00:37:28 +0200

    feat: test communication frontend vers backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/ping', function () {
+    return response()->json(['message' => 'API OK']);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,19 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <p>{{ message }}</p>
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const message = ref('Chargement...')
+
+onMounted(async () => {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/ping`)
+  const data = await res.json()
+  message.value = data.message
+})
+</script>
 
 <style scoped></style>
